adb738901825c3a051f58b3016eef028542fc769
travp624 <travp624@gmail.com>
2013-01-15 17:12:30 -0600

    Toroplus: call the correct files now aosp way
    Signed-off-by: travp624 <travp624@gmail.com>

--- toroplus/device-vendor.mk
+++ toroplus/device-vendor.mk
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-LOCAL_STEM := toroplus/device-toroplus.mk
+LOCAL_STEM := toroplus/device-partial.mk
 
 $(call inherit-product-if-exists, vendor/broadcom/$(LOCAL_STEM))
 $(call inherit-product-if-exists, vendor/csr/$(LOCAL_STEM))
