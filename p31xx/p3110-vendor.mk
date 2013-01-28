3e1c6947401002f6274752e55bc8babd93a4dfb4
nycbjr <nycbjr@gmail.com>
2013-01-28 11:37:45 -0500

    updating p31xx vendor blobs

--- p31xx/p3110-vendor.mk
+++ p31xx/p3110-vendor.mk
@@ -13,6 +13,6 @@
 # limitations under the License.
 
 # Pick up overlay for features that depend on non-open-source files
-DEVICE_PACKAGE_OVERLAYS += vendor/samsung/p31xx/p3110/overlay
+DEVICE_PACKAGE_OVERLAYS := vendor/samsung/p31xx/overlay
 
 $(call inherit-product, vendor/samsung/p31xx/p3110-vendor-blobs.mk)
